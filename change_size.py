cff76a0277eabd282983f10e8380a214ef428924
schatzmana <schatzmana@gmail.com>
2016-06-14 16:22:28 -0400

    takes .tri file, parses out coordinates, changes them to ints, and multiplies them by the scale_index

--- change_size.py
+++ change_size.py
@@ -9,9 +9,51 @@ if len(sys.argv) != 3:
     print "Error: pass in script name, scale percentage, object tri file"
 
 else:
-    scale_index = sys.argv[1]
+    scale_index = float(sys.argv[1])
     tri_file = sys.argv[2]
 
+    myfile = open(tri_file)
+    vert_count = int( myfile.readline() )
+    print "vertice count: %s" %vert_count
+
+    # read each line for the next vert_count lines
+    # parse each line into the three x,y,z values
+    # multiply each of the three coordinates by scale_index
+    # either save these new values or write them to a new file
+
+    # creates list of list of vertices
+    # [ vert1, vert2, vert3, etc...]
+    # where vert1 = ['x', 'y', 'z']
+    vertices = []
+    for each in range(0, vert_count):
+        coordinate = myfile.readline().strip('\n')
+        vertices.append( coordinate.split() )
+    #print vertices
+
+    for vert in range(len(vertices)):
+        x = int( vertices[vert][0] )
+        y = int( vertices[vert][1] )
+        z = int( vertices[vert][2] )
+        print x,y,z
+
+        x *= scale_index
+        y *= scale_index
+        z *= scale_index
+        print x,y,z
+
+"""
+        str_xyz = str(x) +" " + str(y) + " " + str(y)
+        print str_xyz
+        myfile.write(str_xyz)
+"""
+    tri_count = int( myfile.readline() )
+    print "tri_count: %s" %tri_count
+    # maybe parse triangles and scale them too?
+
+    #
+
+
+"""
     if scale_index == 0:
         print "You entered zero. This will not change the size of the object"
     elif scale_index <0:
@@ -20,7 +62,7 @@ else:
         print "You are enlarging the object"
     else:
         print "Number is not comparable to ints, may be a string"
-
+"""
 """
     my_file = open(tri_file, "r+")
     vertice_count = my_file.readline()
@@ -30,3 +72,4 @@ else:
 
     my_file.close
 """
+# added a comment
