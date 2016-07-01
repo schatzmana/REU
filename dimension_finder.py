df14bda9fe0e9de8319a9aa06f9b4413412f0c87
schatzmana <schatzmana@gmail.com>
2016-07-01 12:44:53 -0400

    correctly compares x,y,z values and prints out min and max for each. Does not write dimensional info to a file. Can easily add that functionality if you just eliminate comments.

--- dimension_finder.py
+++ dimension_finder.py
@@ -1,58 +1,78 @@
-# command line : script name, object tri file, and a file to store dimensions into
 import sys
 
-if len(sys.argv) != 3:
-    print "Error: pass in script name, object tri file, and new file name"
+if len(sys.argv) != 2:
+    print "Error: pass in script name, object tri file" #, and new file name"
 
 else:
     tri_file = sys.argv[1]
     trifile = open(tri_file, "r")
-    newfile = open(sys.argv[2], "w") # will write to the file name that user passes in
+    # newfile = open(sys.argv[2], "w") # will write to the file name that user passes in
 
     vert_count = int( trifile.readline() )
     vertices = []
-    inf = float('inf')
-    maxX = -1*inf
-    maxY = -1*inf
-    maxZ = -1*inf
-    minX = inf
-    minY = inf
-    minZ = inf
+    infinit = float('inf')
+    neginf = -1*infinit
+
+    maxX = neginf
+    maxY = neginf
+    maxZ = neginf
+    minX = infinit
+    minY = infinit
+    minZ = infinit
 
     for each in range(0, vert_count):
         coordinate = trifile.readline().strip('\n')
         vertices.append( coordinate.split() )
-         # appends coordinates to the vertices list, with all three coordinates in seperate nested lists
-        #print "just before comparing the x val"
 
-        if vertices[each][0] < minX:
-            minX = vertices[each][0]
-        elif vertices[each][0] > maxX:
-            maxX = vertices[each][0]
+        x = float(vertices[each][0])
+        y = float(vertices[each][1])
+        z = float(vertices[each][2])
 
-        if vertices[each][1] < minY:
-            minY = vertices[each][1]
-        elif vertices[each][1] > maxY:
-            maxY = vertices[each][1]
+        if x < minX:
+            minX = x
+        if x > maxX:
+            maxX = x
 
+        if y < minY:
+            minY = y
+        if y > maxY:
+            maxY = y
 
-        if vertices[each][2] > maxZ:
-            maxZ = vertices[each][2]
-        elif vertices[each][2] < minZ:
-            minZ = vertices[each][2]
+        if z < minZ:
+            minZ = z
+        if z > maxZ:
+            maxZ = z
 
     print "minX: %s, maxX: %s" %(minX, maxX)
     print "minY: %s, maxY: %s" %(minY, maxY)
     print "minZ: %s, maxZ: %s" %(minZ, maxZ)
 
-# PROBLEM
-#always picks 1 for max but 10 for min.....
+    """
+    print infinit
+    print neginf
 
+    if 0 < infinit: #T
+        print "0 is less than infinity"
+    if 0 < neginf: #F
+        print "0 is less than neg infinity"
+    if 0 > neginf: #T
+        print "0 is greater than neg infinity"
 
+    if 100000 < infinit: #T
+        print "100,000 is less than infinity"
+    if 100000 > neginf: #T
+        print "100,000 is greater than neg infinity"
 
-'''
-    for vert in range(len(vertices)):
-            x = float( vertices[vert][0] )
-            y = float( vertices[vert][1] )
-            z = float( vertices[vert][2] )
-'''
+    if -255 > neginf: #T
+        print "-255 is greater than neg infinity"
+    if -255 < neginf: #F
+        print "-255 is less than neg infinity"
+    if -255 > infinit: #F
+        print "-255 is greater than infinity"
+
+    print float(1.30538e-16)
+    if float(1.30538e-16)<-2:
+        print "compared a tiny negative exponential to a negative unsuccessfully"
+    if float(1.30538e-16) < infinit:
+        print "setting tiny exponential to min"
+    """
