d6a3f4a09ca21a545bf6252d296dfc36d541898a
schatzmana <schatzmana@gmail.com>
2016-07-27 17:04:13 -0400

    seems to work minus scaling properly

--- scale_dimension_multiply.py
+++ scale_dimension_multiply.py
@@ -1,7 +1,15 @@
-# takes in scale percentage, object tri file, and num of shapes and will write to the newfile
-# the scaled dimensions and compute the ehight, width, and depth of the new scaled object
+# Command line: python, scale_dimension_multiply.py, scale percentage float, object tri file, and int num of object
+
+# Functionality:
+# Takes object tri file and scales it to float user sends in as "scale percentage" and puts that into a new file named to describe the num and scale of object (see "filename" variable below)
+
+# Note: need to have the Part1.txt, Part2.txt, and Part3.txt all in the Python folder as well for making the .rob file
+
+
+# to the newfile then scales dimensions and computes the height, width, and depth of the new scaled object
 # Also creates new file name that describes scale and num of shape
 # makes multiples
+
 # meant to be run from Klampt/Python when objects are in ../data/objects/
 
 import sys
@@ -10,25 +18,30 @@ if len(sys.argv) != 4:
     print "Error: pass in script name, scale percentage, object tri file, num of shapes"
 
 else:
-    """ change_size """
+    """ initializes all the variables and creates file names"""
     scale_index = float(sys.argv[1])
-    # print "scale index= %s" %scale_index
     tri_file = sys.argv[2]
-    print tri_file
     num = int(sys.argv[3])
 
     trifile = open(tri_file, "r+")
-    newfile = open("../data/objects/newfile.tri", "w+")
-    vert_count = int( trifile.readline() )
+    newfile = open("../data/objects/newfile.tri", "w+")     # intermediate file created which will hold scaled values of .tri file (not stacked yet, just 1)
+    vert_count = int( trifile.readline() ) # number of vertices
     newfile.write(str(vert_count)+"\n")
-    #print "vertice count: %s" %vert_count
 
-    # makes filename that tells num of and scale of new shape, so you can find the new .tri file
+    # makes filename that tells num of and scale of new shape, so you can find the new .tri file in data/objects folder easily
     prefix_len = len('../data/objects/')
     tri_len = len(tri_file)
-    shape = tri_file[ prefix_len:tri_len] # i.e. "cube.tri"
-    filename = '../data/objects/' + str(num) + "of_" + str(scale_index) + shape
+    shape = tri_file[ prefix_len:(tri_len-4)] # i.e. "cube.tri"
+    filename = '../data/objects/' + str(num) + "of_" + str(scale_index) + shape +".tri"
+    # ^ filename of new .tri file with scaling + multipling (Stacking) done
 
+    # makes .rob filename : .rob file with ^ filename as the link for the grasped object
+    rob_filename = "../data/robots/" + str(num) + "of_" + str(scale_index) + shape +".rob"
+    geom_link_filename = '../objects/' + str(num) + "of_" + str(scale_index) + shape + '.tri'   # the filename that goes in the geometry links line of .rob
+
+
+
+    """ change_size """
     # read each line for the next vert_count lines
     # parse each line into the three x,y,z values
     # multiply each of the three coordinates by scale_index
@@ -46,23 +59,19 @@ else:
         x = float( vertices[vert][0] )
         y = float( vertices[vert][1] )
         z = float( vertices[vert][2] )
-        # print "originals are: ", x, y, z
 
         # scaling:
         x *= scale_index
         y *= scale_index
         z *= scale_index
-        # print "scaled coords are: ",x,y,z
 
         str_xyz = str(x) +" " + str(y) + " " + str(z)
-        # print "string coords are: " + str_xyz +"\n"
         newfile.write(str_xyz + "\n")
 
     tri_count = int( trifile.readline() )
     newfile.write(str(tri_count)+"\n")
-    # print "tri_count: %s" %tri_count
 
-    triangles = []
+    triangles = []  # same concept as vertices[] above
     # read line by line of triangle coords (verts) & write them
     for each in range(0, tri_count):
         tri_coord = trifile.readline().strip('\n')
@@ -75,10 +84,13 @@ else:
 
         str_abc = str(a) +" " + str(b) + " " + str(c)
         newfile.write(str_abc + "\n")
-    # write out new values to file
 
-    """ dimension finder """
-    # get height
+
+
+    """ finds dimensions """
+    # finds height, width, and depth of object by comparing all x, y, z values to find min and max of each
+    # useful because gets height so you can stack
+
     infinit = float('inf')
     neginf = -1*infinit
 
@@ -89,16 +101,10 @@ else:
     minY = infinit
     minZ = infinit
 
-    newfile.seek(0) # need to start back reading file at beginning, cause already at end from writing
-    new_vertices = []
-    vert_count = int( newfile.readline().strip('\n') )
     for each in range(0, vert_count):
-        vertex = newfile.readline().strip('\n')
-        new_vertices.append( vertex.split() )
-
-        x = float(new_vertices[each][0])
-        y = float(new_vertices[each][1])
-        z = float(new_vertices[each][2])
+        x = float(vertices[each][0])
+        y = float(vertices[each][1])
+        z = float(vertices[each][2])
 
         if x < minX:
             minX = x
@@ -115,73 +121,51 @@ else:
         if z > maxZ:
             maxZ = z
 
-    #print "minX: %s, maxX: %s" %(minX, maxX)
-    #print "minY: %s, maxY: %s" %(minY, maxY)
-    #print "minZ: %s, maxZ: %s" %(minZ, maxZ)
-
     height = maxY-minY
     width = maxX-minX
     depth = maxZ-minZ
 
     print "height: %s, width: %s, depth: %s" %(height, width, depth)
 
-    """ multiply """
-    # take in num shapes
-    writefile = open(filename, "w")
-    newfile.seek(0)
-    writefile.write( str( num * vert_count )+"\n" )
-    # read each line for the next vert_count lines
-    # parse each line into the three x,y,z values
-    # multiply each of the three coordinates by scale_index
-    # either save these new values or write them to a new file
 
-    # creates list of list of vertices
-    # [ vert1, vert2, vert3, etc...]
-    # where vert1 = ['x', 'y', 'z']
-    count = 0
 
+    """ multiply (stacks) """
+    # require command line to include num shapes
+    writefile = open(filename, "w")     # writing the final .tri file (scaled, stacked and all)
+    newfile.seek(0) # need to start back reading file at beginning, cause already at end from writing
+    writefile.write( str( num * vert_count )+"\n" ) # accounts for stacking
+
+    count = 0   # index for stacking
+
+    # writes scaled coordinates
     for each in range(0, vert_count):
-        x = float(new_vertices[each][0])
-        y = float(new_vertices[each][1])
-        z = float(new_vertices[each][2])
+        x = float(vertices[each][0])
+        y = float(vertices[each][1])
+        z = float(vertices[each][2])
         str_xyz = str(x) +" " + str(y) + " " + str(z)
-        #print "string coords are: " + str_xyz +"\n"
-        #print str_xyz
         writefile.write(str_xyz + "\n")
     count += 1
-    print "count is: " + str(count)
+    #print "count is: " + str(count)
     # end of read and write for the first go through
-    # writes scaled coordinates
 
     # now need to read through again and add vals for next stack
-
     """ for loop to add vertices and triangles num times """
     while count < num:
-        for vert in range(len(new_vertices)):
-            x = float( new_vertices[vert][0] )
-            y = float( new_vertices[vert][1] )
-            z = float( new_vertices[vert][2] )
-            #print "originals are: ", x, y, z
+        for vert in range(len(vertices)):
+            x = float( vertices[vert][0] )
+            y = float( vertices[vert][1] )
+            z = float( vertices[vert][2] )
 
             # adding additional height, only edits 1/3 coords (only y), but could edit them all here
-            #x *= scale_index
             y += ( height * count)
-            #z *= scale_index
-            #print "scaled print "count is: " + str(count)coords are: ",x,y,z
 
             str_xyz = str(x) +" " + str(y) + " " + str(z)
-            #print "string coords are: " + str_xyz +"\n"
-            #print str_xyz
             writefile.write(str_xyz + "\n")
         count += 1
-        print "count is: " + str(count)
+        #print "count is: " + str(count)
 
     writefile.write(str(tri_count * num)+"\n")
-    #print "tri_count: %s" %tri_count
 
-    """ stopped here but probably need to go back and read through
-    newfile because need to have some way of knowing the number of triangles
-    in order to continue and alter triangles vertices """
 
     #stores them so can use later without rereading and adjusting seek()
     # read line by line of triangle coords (verts) & write them
@@ -193,11 +177,8 @@ else:
         c = int( triangles[each][2] )
 
         str_abc = str(a) +" " + str(b) + " " + str(c)
-        #print "string coords are: " + str_xyz +"\n"
-        #print str_abc
         writefile.write(str_abc + "\n")
     count += 1
-    # write out new values to file
 
     """ Repeat ^ to adjust vertices by object's height """
     while count < num:
@@ -207,13 +188,40 @@ else:
             c = int( triangles[each][2] )
 
             # to make new triangles correspond to the correct vertices from the originals
-            # need to just add on total count
+            # need to just add on total count to the previous on each iter through
             a += (vert_count * count)
             b += (vert_count * count)
             c += (vert_count * count)
 
             str_abc = str(a) +" " + str(b) + " " + str(c)
-            #print "string coords are: " + str_xyz +"\n"
-            #print str_abc
             writefile.write(str_abc + "\n")
         count += 1
+
+
+    """ write new .rob file with above constructed filename """
+    # ADD FUNCTIONALITY FOR CENTERING OBJECTS ON HAND FOR OTHER OBJECTS HERE
+    # only knows how to center with cubes thus far...
+    if tri_file == "../data/objects/cube.tri": #may need to switch this later to allow access from different directories
+        j = float(scale_index) * -0.5
+        k = float(scale_index) * -0.5
+        l = -(float(scale_index) + 0.11)
+    else: #temporary, change this later!!
+        j = 0
+        k = 0
+        l = 0
+
+    # makes the .rob
+    rob_newfile = open(rob_filename, "w")
+    robtext = open("./Part1.txt", 'r')
+    rob_newfile.write( robtext.read() )
+
+    robtext2 = open("./Part2.txt", 'r')
+    jkl = str(j)+" "+str(k)+" "+str(l)+"\n"
+    last_tv = "1 0 0 0 1 0 0 0 1 "+jkl  # last translational vector (aka for the additional link, the grasped object)
+    rob_newfile.write( last_tv )
+    rob_newfile.write( robtext2.read() +"\n")
+
+    robtext3 = open("./Part3.txt", 'r')
+    links = str('geometry	"jaco/jaco_link_base.tri" "jaco/jaco_link_1.tri" "jaco/jaco_link_2.tri" "jaco/jaco_link_3.tri" "jaco/jaco_link_4.tri" "jaco/jaco_link_5.tri" "jaco/jaco_link_hand.tri" "jaco/jaco_link_finger_1.tri" "jaco/jaco_link_finger_2.tri" "jaco/jaco_link_finger_3.tri" ')+'"'+ geom_link_filename + '"'
+    rob_newfile.write( links + "\n")
+    rob_newfile.write( robtext3.read() )
